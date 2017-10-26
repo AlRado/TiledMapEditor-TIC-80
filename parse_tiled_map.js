efebbda6e8136ea464ce32689ac64f0b97ab1042
al_rado <alrado08@gmail.com>
2017-10-26 19:05:09 +0300

    hotfix: indexes tiles are set incorrectly

--- parse_tiled_map.js
+++ parse_tiled_map.js
@@ -2,7 +2,7 @@ const fs = require('fs'),
       SIZE = 32640
 let rawData = require('./world_map.json').layers[0].data;
 
-let data = Array.from(rawData).map(e => Math.min(e - 1, 0));
+let data = Array.from(rawData).map(x => Math.max(x - 1, 0));
 data.length = SIZE;
 
 fs.writeFileSync('./world_map.map', Buffer.from(data))

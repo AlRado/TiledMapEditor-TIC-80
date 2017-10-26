f648b0f4b1f45afd0120619050c0b0040fe06a1b
al_rado <alrado08@gmail.com>
2017-10-26 18:53:57 +0300

    added example files: tileset.tsx, .json, .map

---
+++ tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sprites" tilewidth="8" tileheight="8" tilecount="512" columns="16">
+ <image source="sprites.gif" width="128" height="256"/>
+</tileset>

76718e2c47c3473e37b8ae80f6c49875b4533a56
al_rado <alrado08@gmail.com>
2017-10-28 23:21:04 +0300

    hotfix: the parentheses are interchanged

--- tic2tiled.js
+++ tic2tiled.js
@@ -1,50 +1,50 @@
 const fs = require('fs'),
-      xml2js = require('xml2js'),
-      parser = new xml2js.Parser(),
-      SIZE = 32640,
-      converts = {
-  'map->tmx': function(str) {
-    return new Promise((res, rej) => {
-      fs.readFile('default_data/default_map.tmx', 'utf-8', (err, data) => {
-        if(err) {
-          rej(err);
-          return;
-        }
-        parser.parseString(data, (err, result) => {
-          if(err) {
+  xml2js = require('xml2js'),
+  parser = new xml2js.Parser(),
+  SIZE = 32640,
+  converts = {
+    'map->tmx': function (str) {
+      return new Promise((res, rej) => {
+        fs.readFile('default_data/default_map.tmx', 'utf-8', (err, data) => {
+          if (err) {
             rej(err);
             return;
           }
+          parser.parseString(data, (err, result) => {
+            if (err) {
+              rej(err);
+              return;
+            }
+            let tiles = result.map.layer[0].data[0].tile;
+            let dataView = new DataView(str);
+            for (var i = 0; i < SIZE; i++) {
+              tiles[i] = { '$': { gid: (dataView.getInt8(i) + 1).toString() } }
+            }
+            res((new xml2js.Builder()).buildObject(result));
+          })
+        })
+      });
+    },
+    'tmx->map': function (str) {
+      return new Promise((res, rej) => {
+        parser.parseString(str, (err, result) => {
+          if (err) rej(err);
           let tiles = result.map.layer[0].data[0].tile;
-          let dataView = new DataView(str);
-          for (var i = 0; i < SIZE; i++) {
-            tiles[i] = { '$': { gid: (dataView.getInt8(i) + 1).toString() } }
+          let data = new Int8Array(SIZE);
+          for (var i = 0; i < data.length; i++) {
+            data[i] = tiles[i].$.gid - 1;
           }
-          res((new xml2js.Builder()).buildObject(result));
+          res(Buffer.from(data));
         })
       })
-    });
-  },
-  'tmx->map': function(str) {
-    return new Promise((res, rej) => {
-      parser.parseString(str, (err, result) => {
-        if(err) rej(err);
-        let tiles = result.map.layer[0].data[0].tile;
-        let data = new Int8Array(SIZE);
-        for (var i = 0; i < data.length; i++) {
-          data[i] = tiles[i].$.gid - 1;
-        }
-        res(Buffer.from(data));
-      })
     }
-    });
-};
+  };
 let string, typeIn, typeOut, readed;
 module.exports = {
   file: function file(filename) {
     readed = new Promise((res, rej) => {
       fs.readFile(filename, 'utf-8', (err, data) => {
-        if(err) rej(err);
+        if (err) rej(err);
         else res(data);
       })
     })
@@ -56,31 +56,31 @@ module.exports = {
   },
   from: function from(type) {
     typeIn = (typeof type == 'string') ? type : type.toString();
-    if(
+    if (
       (typeof typeOut == 'string') &&
       (typeOut.length > 1) &&
       (typeIn.length > 1)
     ) {
-      if(!(`${typeIn}->${typeOut}` in converts)) throw new Error('No such convertation');
+      if (!(`${typeIn}->${typeOut}` in converts)) throw new Error('No such convertation');
     }
     return this;
   },
   to: function to(type) {
     typeOut = (typeof type == 'string') ? type : type.toString();
-    if(
+    if (
       (typeof typeIn == 'string') &&
       (typeIn.length > 1) &&
       (typeOut.length > 1)
     ) {
-      if(!(`${typeIn}->${typeOut}` in converts)) throw new Error('No such convertation');
+      if (!(`${typeIn}->${typeOut}` in converts)) throw new Error('No such convertation');
     }
     return this;
   },
   out: function out(filename) {
-    if(typeof string == 'string') {
+    if (typeof string == 'string') {
       return new Promise((res, rej) => {
         fs.writeFile(filename, converts[`${typeIn}->${typeOut}`](string), (err) => {
-          if(err) rej(err);
+          if (err) rej(err);
           else res();
         })
         delete string;
@@ -91,7 +91,7 @@ module.exports = {
           .then((string) => converts[`${typeIn}->${typeOut}`](string))
           .then((string) => {
             fs.writeFile(filename, string, (err) => {
-              if(err) rej(err);
+              if (err) rej(err);
               else res();
             })
           }, (err) => {

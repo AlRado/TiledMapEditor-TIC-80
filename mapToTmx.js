e5c3556dfdb4097bf6c6b82f1e7c5755648fef6d
DarkHole1 <dark.hole1@yandex.ru>
2017-10-28 17:46:15 +0300

    Shortier ;)

--- mapToTmx.js
+++ mapToTmx.js
@@ -1,22 +1,15 @@
 const SIZE = 32640,
     MAP_NAME = 'world_map';
-var fs = require('fs'),
+const fs = require('fs'),
     xml2js = require('xml2js');
 
-fs.readFile('./' + MAP_NAME + '.map', function (err, rawData) {
-    fs.readFile('./default_data/default_map.tmx', function (err, data) {
-        let parser = new xml2js.Parser();
-        parser.parseString(data, function (err, result) {
-            let tiles = result.map.layer[0].data[0].tile;
-            let dataView = new DataView(rawData.buffer);
-            for (var i = 0; i < SIZE; i++) {
-                tiles[i] = { '$': { gid: (dataView.getInt8(i) + 1).toString() } }
-            }
-            let builder = new xml2js.Builder();
-            let xml = builder.buildObject(result);
-            fs.writeFileSync('./' + MAP_NAME + '.tmx', xml);
+(new xml2js.Parser()).parseString(fs.readFileSync('./default_data/default_map.tmx'), function (err, result) {
+  let tiles = result.map.layer[0].data[0].tile;
+  let dataView = new DataView(fs.readFileSync(`./${MAP_NAME}.map`).buffer);
+  for (var i = 0; i < SIZE; i++) {
+    tiles[i] = { '$': { gid: (dataView.getInt8(i) + 1).toString() } }
+  }
+  fs.writeFileSync(`./${MAP_NAME}.tmx`, (new xml2js.Builder()).buildObject(result));
 
-            console.log(MAP_NAME + '.map converted to ' + MAP_NAME + '.tmx');
-        });
-    });
+  console.log(`${MAP_NAME}.map converted to ${MAP_NAME}.tmx`);
 });

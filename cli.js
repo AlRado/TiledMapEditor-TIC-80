bd0cd5bba44075425375a7655e340c4706025a30
al_rado <alrado08@gmail.com>
2017-10-29 14:52:19 +0300

    added cli.js

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const args = require('yargs').argv;
+if (args._.length < 2) {
+  console.error('Usage:\ntic2tiled [options] file.inExt file.outExt');
+  process.exit(1);
+}
+const path = require('path'),
+  fileIn = args._[0],
+  fileInExt = path.extname(fileIn),
+  fileOut = args._[1],
+  fileOutExt = path.extname(fileOut)
+
+require('./tic2tiled').file(`./${fileIn}`).from(fileInExt).to(path.extname(fileOut)).out(`./${fileOut}`);
